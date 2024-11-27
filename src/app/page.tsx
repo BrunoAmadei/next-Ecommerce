c41e05b924c94248bcdb3c5ed8d65a4ae42849a5
BrunoAmadei <brunoamadei98@gmail.com>
2024-11-27 19:00:17 -0300

    teste layout de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Titulo super maneiro</h1>
-    </main>
+    <div className="max-w-7xl nx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-white rounded-lg overflow-hidden">
+          prod 1
+        </div>
+        <div className="bg-white rounded-lg overflow-hidden">
+          prod 1
+        </div>
+
+        <div className="bg-white rounded-lg overflow-hidden">
+          prod 1
+        </div>
+
+        <div className="bg-white rounded-lg overflow-hidden">
+          prod 1
+        </div>
+
+        <div className="bg-white rounded-lg overflow-hidden">
+          prod 1
+        </div>
+      </div>
+    </div>
   );
 }
