928ee7f50e4ec8fe6b30226a626d0e7c19ff9884
BrunoAmadei <brunoamadei98@gmail.com>
2024-11-28 18:57:26 -0300

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,25 @@
-import Image from "next/image";
+import { ProductType } from "@/types/ProductType";
 
-export default function Home() {
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  if (!res.ok) {
+    throw new Error('falha em fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const products = await getProducts()
+
   return (
     <div className="max-w-7xl nx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        <div className="bg-white rounded-lg overflow-hidden">
-          prod 1
-        </div>
-        <div className="bg-white rounded-lg overflow-hidden">
-          prod 1
-        </div>
-
-        <div className="bg-white rounded-lg overflow-hidden">
-          prod 1
-        </div>
-
-        <div className="bg-white rounded-lg overflow-hidden">
-          prod 1
-        </div>
-
-        <div className="bg-white rounded-lg overflow-hidden">
-          prod 1
-        </div>
+        {products.map((product: ProductType) => {
+            <div key={product.id}>
+              {product.title}
+            </div>
+        })}
       </div>
     </div>
   );
